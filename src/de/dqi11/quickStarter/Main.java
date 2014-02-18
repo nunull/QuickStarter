151da7a001e1242f36307263a29fa5a6771aa6c5
nunull <timmm.albers@gmail.com>
2014-02-18 12:53:28 +0100

    Initial sourcecode-commit. Creates main-package.

---
+++ src/de/dqi11/quickStarter/Main.java
@@ -0,0 +1,13 @@
+package de.dqi11.quickStarter;
+
+public class Main {
+
+	/**
+	 * @param args ignored
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
