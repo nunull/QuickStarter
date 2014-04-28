3c99f06ff32ce3937022dc962e3e6c7b55782fde
dqi11uelker <y_uelker@hotmail.de>
2014-04-28 13:17:14 +0200

    implemented a loop when an error occurs, to write a " " instead of a "," is possible now

--- src/de/dqi11/quickStarter/modules/WeatherModule.java
+++ src/de/dqi11/quickStarter/modules/WeatherModule.java
@@ -1,6 +1,7 @@
 package de.dqi11.quickStarter.modules;
 
 import java.net.ConnectException;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 
 import javax.swing.SwingWorker;
@@ -12,22 +13,38 @@ import de.dqi11.quickStarter.modules.bridges.OpenWeatherMapBridge;
 
 public class WeatherModule extends Module {
 	private final String KEY = this.toString();
-	
+	private SwingWorker<ModuleAction, ModuleAction> worker;
+
 	public WeatherModule(MainController mainController) {
 		super(mainController);
+		worker = new SwingWorker<ModuleAction, ModuleAction>(){
+			@Override
+			protected ModuleAction doInBackground() throws Exception {
+				return null;
+			}
+		};
 	}
-	
+
 	@Override
 	public ModuleAction getModuleAction(final Search search) throws ConnectException {
+
 		if (search.isCommand("weather")) {
-			SwingWorker<ModuleAction, ModuleAction> worker = new SwingWorker<ModuleAction, ModuleAction>() {
-				
+			try{
+				worker.cancel(true);
+			}catch(CancellationException e){
+				//TODO
+			}
+			worker = new SwingWorker<ModuleAction, ModuleAction>() {
+
 				@Override
 				protected ModuleAction doInBackground() throws Exception {
 					String location = search.getParam(0);
 					if(location == null) location = "Bremen";
-					if(location.split(",").length == 1) location += ",de";
-					
+					if(location.split(",").length == 1){
+						if (search.getParam(1)!=null) location +=","+search.getParam(1);
+						else location += ",de";
+					}
+
 					String json = OpenWeatherMapBridge.getJSON(location, 2000);
 					String text = "";
 					JSONParser jsonParser = new JSONParser(json);
@@ -35,23 +52,29 @@ public class WeatherModule extends Module {
 					
 					return new ModuleAction(KEY, text);
 				}
-				
+
 				@Override
 				protected void done() {
 					try {
+						System.out.println(search.getSearchString());
 						getMainController().updateModule(get());
 					} catch (InterruptedException | ExecutionException e) {
 						getMainController().updateModule(new WarningModuleAction(KEY, "An error occured."));
+
+						try {
+							getModuleAction(search);
+						} catch (ConnectException e1) {
+							e1.printStackTrace();
+						}
 					}
-					
 					super.done();
 				}
 			};
 			worker.execute();
-			
+
 			return new LoadingModuleAction(KEY, "Loading weather...");
 		}
-		
+
 		return null;
 	}
 
