0d781fa6b2ffabe1d5bb6d336ada9ac710d35a0f
dqi11uelker <y_uelker@hotmail.de>
2014-05-05 14:04:56 +0200

    the command "img" lets the GoogleModule search in google picures #3

--- src/de/dqi11/quickStarter/modules/GoogleSearchModule.java
+++ src/de/dqi11/quickStarter/modules/GoogleSearchModule.java
@@ -12,6 +12,7 @@ import de.dqi11.quickStarter.controller.Search;
 
 public class GoogleSearchModule extends Module {
 	private final String KEY = this.toString();
+	private String adtCmd = null;
 	
 	public GoogleSearchModule(MainController mainController) {
 		super(mainController);
@@ -20,12 +21,15 @@ public class GoogleSearchModule extends Module {
 	@Override
 	public ModuleAction getModuleAction(final Search search) {
 		if(!search.getSearchString().equals("")) {
+			if (search.isCommand("img")) adtCmd = "&tbm=isch";
+			else adtCmd = null;
+			
 			return new ModuleAction(KEY, "Google for <b>" + search.getSearchString() + "</b>", new ImageIcon("res/google-logo.png")) {
 				@Override
 				public void invoke(Search search) {
 					if(Desktop.isDesktopSupported()) {
 						try {
-							Desktop.getDesktop().browse(new URI("https://www.google.com/#q=" + search.getSearchString().replaceAll(" ", "+")));
+							Desktop.getDesktop().browse(new URI("https://www.google.com/#q=" + search.getSearchString().replaceAll(" ", "+") + adtCmd));
 						} catch (IOException | URISyntaxException e) {
 							getMainController().updateModule(new WarningModuleAction(KEY, "An error occured."));
 						}
