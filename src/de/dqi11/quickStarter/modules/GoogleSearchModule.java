737ff871bcfadbc4cbc8d70866df120e24eb5020
dqi11uelker <y_uelker@hotmail.de>
2014-05-05 14:12:20 +0200

    included video search (command: "vid" or "video") #close #3

--- src/de/dqi11/quickStarter/modules/GoogleSearchModule.java
+++ src/de/dqi11/quickStarter/modules/GoogleSearchModule.java
@@ -22,7 +22,9 @@ public class GoogleSearchModule extends Module {
 	public ModuleAction getModuleAction(final Search search) {
 		if(!search.getSearchString().equals("")) {
 			if (search.isCommand("img")) adtCmd = "&tbm=isch";
+			else if (search.isCommand("vid") || search.isCommand("video")) adtCmd = "&tbm=vid";
 			else adtCmd = null;
+			
 			
 			return new ModuleAction(KEY, "Google for <b>" + search.getSearchString() + "</b>", new ImageIcon("res/google-logo.png")) {
 				@Override
