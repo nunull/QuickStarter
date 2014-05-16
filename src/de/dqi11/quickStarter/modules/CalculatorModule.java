72603cef508671e9989703c23321e539d5822879
nunull <timmm.albers@gmail.com>
2014-05-16 13:52:54 +0200

    Improves CalculatorModule

--- src/de/dqi11/quickStarter/modules/CalculatorModule.java
+++ src/de/dqi11/quickStarter/modules/CalculatorModule.java
@@ -1,10 +1,7 @@
 package de.dqi11.quickStarter.modules;
 
-import java.awt.Color;
 import java.awt.Dimension;
 import java.net.ConnectException;
-
-import javax.swing.JPanel;
 
 import org.math.plot.Plot2DPanel;
 import org.matheclipse.parser.client.SyntaxError;
@@ -25,33 +22,38 @@ public class CalculatorModule extends Module {
 
 	@Override
 	public ModuleAction getModuleAction(Search search) throws ConnectException {
-		if(search.getSearchString().startsWith("f(x)")) {
+		if(search.getSearchString().matches("[a-zA-Z]\\([a-zA-Z]\\).*")) {
 			return new ModuleAction(KEY, "Draw <b>" + search.getSearchString() + "</b>") {
 				@Override
 				public ModuleWindow getModuleWindow(Search search) {
-					ComplexEvaluator evaluator = new ComplexEvaluator();
-					String expression = search.getSearchString().split("=")[1].trim();
-					System.out.println(expression);
-					
-					ModuleWindow moduleWindow = new ModuleWindow();
-					Plot2DPanel plot = new Plot2DPanel();
-					plot.setPreferredSize(new Dimension(770, 490));
-					plot.plotToolBar.hide();
-					
-					double[] x = new double[100];
-					double[] y = new double[100];
-					for(int i = 0, j = x.length; i < j; i++) {
-						evaluator.defineVariable("x", new ComplexVariable(i-50));
+					try {
+						ComplexEvaluator evaluator = new ComplexEvaluator();
+						String variableName = new Character(search.getSearchString().charAt(2)).toString();
+						String expression = search.getSearchString().split("=")[1].trim();
+						System.out.println(variableName);
 						
-						x[i] = i-50;
-						y[i] = evaluator.evaluate(expression).getReal();
+						ModuleWindow moduleWindow = new ModuleWindow();
+						Plot2DPanel plot = new Plot2DPanel();
+						plot.setPreferredSize(new Dimension(770, 490));
+						plot.plotToolBar.setVisible(false);
+						
+						double[] x = new double[100];
+						double[] y = new double[100];
+						for(int i = 0, j = x.length; i < j; i++) {
+							evaluator.defineVariable("x", new ComplexVariable(i-50));
+							
+							x[i] = i-50;
+							y[i] = evaluator.evaluate(expression).getReal();
+						}
+						
+						plot.addLinePlot(search.getSearchString(), x, y);
+						
+						moduleWindow.add(plot);
+						
+						return moduleWindow;
+					} catch(IndexOutOfBoundsException e) {
+						return null;
 					}
-					
-					plot.addLinePlot(search.getSearchString(), x, y);
-					
-					moduleWindow.add(plot);
-					
-					return moduleWindow;
 				}
 			};
 		} else {
