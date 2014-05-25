8b0552c45906850414fe9ee6d29ba7e96f12c821
Yunus <y_uelker@hotmail.de>
2014-05-25 10:22:54 +0200

    added empty PreferenceWindow, added abstract class Window

---
+++ src/de/dqi11/quickStarter/gui/PreferenceWindow.java
@@ -0,0 +1,7 @@
+package de.dqi11.quickStarter.gui;
+/**
+ * A class to show and change the preferences of the program, including the modules.
+ */
+public class PreferenceWindow extends Window {
+	
+}
