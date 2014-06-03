a5adc29dd5e75ee8ff03fe802c4284d18aaf23b5
dqi11uelker <y_uelker@hotmail.de>
2014-06-03 12:31:13 +0200

    TranslateModule includes the GooleTranslateModule now

--- src/de/dqi11/quickStarter/modules/TranslateModule.java
+++ src/de/dqi11/quickStarter/modules/TranslateModule.java
@@ -1,6 +1,10 @@
 package de.dqi11.quickStarter.modules;
 
+import java.awt.Desktop;
+import java.io.IOException;
 import java.net.ConnectException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 
@@ -13,9 +17,13 @@ import de.dqi11.quickStarter.modules.bridges.GlosbeBridge;
 
 public class TranslateModule extends Module {
 	private final String KEY = this.toString();
+	private String defaultSrcLanguage;
+	private String systemLanguage = Locale.getDefault().getCountry();
 
 	public TranslateModule(MainController mainController) {
 		super(mainController);
+		defaultSrcLanguage = mainController.getModuleProperty(this, "defaultSrcLanguage");
+		systemLanguage = Locale.getDefault().getCountry();
 	}
 
 	@Override
@@ -29,23 +37,39 @@ public class TranslateModule extends Module {
 				protected ModuleAction doInBackground() throws Exception {
 					String from, to, phrase;
 					
-					phrase = search.getParam(0);
+					phrase = search.getParam(0); 
+					if (phrase.equals("")) 
+						return new ModuleAction(KEY, "type the word to translate");
+					
 					from = search.getParam(1);
-					if (from==null) from = "eng";
+					if (from.equals("")) from = defaultSrcLanguage;
 					to = search.getParam(2);
-					if (to==null) to = Locale.getDefault().getLanguage();
+					if (to.equals("")) to = Locale.getDefault().getLanguage();
 					
 					JSONParser parser = new JSONParser( GlosbeBridge.getJSON(from, to, phrase) );
 					
-					return new ModuleAction(KEY, parser.getArrayList("tuc").get(0).get("phrase.text"));
+					return new ModuleAction(KEY, parser.getArrayList("tuc").get(0).get("phrase.text")){
+						@Override
+						public void invoke(Search search) {
+							if(Desktop.isDesktopSupported()) {
+								try {
+									if(systemLanguage != null)
+										Desktop.getDesktop().browse(new URI("https://translate.google.com/#auto/" + systemLanguage + "/" + search.getParams().replaceAll(" ", "+")));
+									else
+										Desktop.getDesktop().browse(new URI("https://translate.google.com/#auto/en/" + search.getParams().replaceAll(" ", "+")));
+								} catch (IOException | URISyntaxException e) {
+									getMainController().updateModule(new WarningModuleAction(KEY, "An error occured."));
+								}
+							}
+						}
+					};
 				}
 				
 				protected void done() {
 					try {
 						getMainController().updateModule(get());
 					} catch (InterruptedException | ExecutionException e) {
-						e.printStackTrace();
-						getMainController().updateModule(new WarningModuleAction(KEY, "An error occured."));
+						getMainController().updateModule(new WarningModuleAction(KEY, "No translation found."));
 					}
 					
 					super.done();
@@ -57,6 +81,10 @@ public class TranslateModule extends Module {
 		}
 		
 		return null;
+	}
+
+	public String getDefaultSrcLanguage() {
+		return defaultSrcLanguage;
 	}
 	
 }
