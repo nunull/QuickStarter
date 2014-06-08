c41d3738dd3c054c871c776c4ea6f39cde17b7fc
nunull <timmm.albers@gmail.com>
2014-06-08 20:38:34 +0200

    Removes umlauts to fix the automatic build

--- src/de/dqi11/quickStarter/modules/WeatherModule.java
+++ src/de/dqi11/quickStarter/modules/WeatherModule.java
@@ -3,7 +3,7 @@
  * 
  * The MIT License (MIT)
  *
- * Copyright (c) 2014 Timm Albers, Arne Peschken, Yunus †lker
+ * Copyright (c) 2014 Timm Albers, Arne Peschken, Yunus Uelker
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
